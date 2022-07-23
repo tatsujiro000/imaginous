29ecf4f9af89439ccc44b75aa8a813086852f5dd
tatsujiro000 <t.oshikiri000@gmail.com>
2022-07-24 02:41:21 +0900

    add-auth

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,28 @@
 import { Outlet, NavLink } from "react-router-dom";
 import './App.css';
+import { AuthProvider } from './context/authContext';
 
 
 export default function App() {
   return (
-    <div>
-      <h1>IMAGINOUS</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/top">Top</NavLink> |{" "}
-        <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/mypage">Mypage</NavLink> |{" "}
-        <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/chat">Chat</NavLink> |{" "}
-        <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/login">Login</NavLink> |{" "}
-        <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/signup">signup</NavLink> |{" "}
-      </nav>
-      <Outlet />
+    <AuthProvider>
+      <div>
+        <h1>IMAGINOUS</h1>
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+        >
+          <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/top">Top</NavLink> |{" "}
+          <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/mypage">Mypage</NavLink> |{" "}
+          <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/chat">Chat</NavLink> |{" "}
+          <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/login">Login</NavLink> |{" "}
+          <NavLink className={({ isActive }) => isActive ? "activeLink" : ""} to="/signup">signup</NavLink> |{" "}
+        </nav>
+        <Outlet />
 
-    </div>
+      </div>
+    </AuthProvider>
   );
 }
--- front/src/routes/login.jsx
+++ front/src/routes/login.jsx
@@ -1,7 +1,67 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRef,useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+
+
 export default function Login() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+
+
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const auth = getAuth();
+
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
+      console.log(emailRef.current.value, passwordRef.current.value);
+      console.log(email, password);
+      await signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          console.log("ログイン");
+          navigate('/top');
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setError(error.message);
+          console.log(errorCode);
+          console.log(errorMessage);
+          console.log("失敗");
+        });
+    };
+
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Login</h2>
+        {error && <p style={{color:'red'}}>{error}</p>}
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>メールアドレス</label>
+              <input name="email" type="email" placeholder="email" ref={emailRef} />
+            </div>
+            <div>
+              <label>パスワード</label>
+              <input name="password" type="password" ref={passwordRef} />
+            </div>
+            <div>
+              <button>ログインする</button>
+            </div>
+            <div>
+              ユーザー登録は<Link to={'/signup'}>こちら</Link>
+            </div>
+          </form>
+        </div>
       </main>
     );
 }
+
+
+
